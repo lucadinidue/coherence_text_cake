c4e650483edd8ae259cd695d0f0bff8cf705dcf0
lucadinidue <luca.dini16@gmail.com>
2024-01-29 16:23:05 +0100

    added script to print normalized confusion matrix

--- scripts/evaluation/models_evaluation.py
+++ scripts/evaluation/models_evaluation.py
@@ -5,6 +5,12 @@ import os
 
 sns.set(style='darkgrid')
 
+lr_map = {
+    'deberta-v3-xsmall': 4.5e-5,
+    'deberta-v3-small': 4.5e-5,
+    'deberta-v3-base': 2e-5
+}
+
 
 def load_labels_from_file(src_path):
     labels_list = []
@@ -21,9 +27,9 @@ def compute_model_f1(results_dir):
 
 
 def load_model_results(results_df, model_dir, model_name):
-    model_configs_dirs = [dir_name for dir_name in os.listdir(model_dir) if dir_name != 'prove']
+    model_configs_dirs = [dir_name for dir_name in os.listdir(model_dir)]  # if dir_name != 'prove']
     for model_config in model_configs_dirs:
-        dataset = model_config.split('_')[2]
+        dataset = model_config.split('_')[1]
         results_dir = os.path.join(model_dir, model_config, 'predictions')
         model_result = compute_model_f1(results_dir)
         new_row = pd.DataFrame.from_dict({'dataset': [dataset], 'model': [model_name], 'score': [model_result]})
@@ -34,13 +40,13 @@ def load_model_results(results_df, model_dir, model_name):
 def main():
     models_dir = '../models'
     results_df = pd.DataFrame(columns=['dataset', 'model', 'score'])
-    for model_name in os.listdir(models_dir):
-        model_dir = os.path.join(models_dir, model_name)
+    for model_name in [model_name for model_name in os.listdir(models_dir) if model_name !='deberta-v3-large']:
+        model_dir = os.path.join(models_dir, model_name, f'{lr_map[model_name]}')
         results_df = load_model_results(results_df, model_dir, model_name)
     bar_plot = sns.barplot(results_df, x='dataset', y='score', hue='model')
     sns.move_legend(bar_plot, 'lower center', bbox_to_anchor=(1, 1))
     fig = bar_plot.get_figure()
-    fig.savefig('results/model_performances.png', bbox_inches='tight')
+    fig.savefig('results/model_performances_lr_huggingface.png', bbox_inches='tight')
 
 
 if __name__ == '__main__':
