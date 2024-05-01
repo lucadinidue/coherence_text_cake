b95cfa90192089c33d7856d639dc2b54595a7981
Luca Dini <luca.dini16@gmail.com>
2024-05-01 10:20:44 +0200

    Delete scripts/data_processing/apply_perturbations.sh

--- scripts/data_processing/apply_perturbations.sh
+++
@@ -1,6 +0,0 @@
-# ENGLISH
-
-# python perturb_paragraphs_all.py -d wiki -l en
-python perturb_paragraphs_all.py -d news -l en
-python perturb_paragraphs_all.py -d fanfic -l en
-python perturb_paragraphs_all.py -d ted -l en
